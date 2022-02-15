a0dbf9786e3af1628b970496bd4e3ccebeed3eae
Terrell Russell <terrellrussell@gmail.com>
2022-02-14 22:56:08 -0500

    new year!

--- src/const.rkt
+++ src/const.rkt
@@ -5,7 +5,7 @@
 (define molasses-site "https://github.com/jjsimpso/molasses")
 
 (define about-version-string
-  (format "Molasses version ~a~nCopyright (C) 2021 Jonathan Simpson~n~nSource code and updates available at ~a"
+  (format "Molasses version ~a~nCopyright (C) 2022 Jonathan Simpson~n~nSource code and updates available at ~a"
           molasses-version
           molasses-site))
 
