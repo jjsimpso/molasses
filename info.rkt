592706ed27707139e4a79f8f45beedb6f169af28
Jonathan Simpson <jjsimpso@gmail.com>
2025-11-24 23:07:38 -0500

    Bump version to 0.7.5. Improve cross compile make targets.
      - add macos arm target
      - fix cross compile dependency commands so they don't fail if
        dependencies have already been installed

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version "0.7.4")
+(define version "0.7.5")
 (define deps (list "base" "gui-lib" "magic" "sxml" "mcfly"
                    "data-lib"
                    "draw-lib"
